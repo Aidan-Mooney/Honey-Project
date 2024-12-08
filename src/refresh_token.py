920603a4124ba1da91f39e711c6cb65b733978dd
Aidan-Mooney <aidanmooney2001@gmail.com>
2024-12-08 15:23:06 +0000

    change from returning status code as well as body to raising a HTTPError

--- src/refresh_token.py
+++ src/refresh_token.py
@@ -1,5 +1,4 @@
 from datetime import datetime as dt
-import logging
 
 
 from src.get_token import get_token
@@ -7,15 +6,10 @@ from src.get_token import get_token
 
 def refresh_token(token_url, auth_id, auth_secret, start_time, time_till_expiry):
     if start_time + time_till_expiry <= dt.now().timestamp():
-        token_code, token_body = get_token(token_url, auth_id, auth_secret)
+        token_response = get_token(token_url, auth_id, auth_secret)
         start_time = dt.now().timestamp()
-        if token_code != 200:
-            logging.warning(
-                f"HTTP code: {token_code}, body returned: {token_body} when calling get_token."
-            )
-            return "warning"
-        token = token_body["access_token"]
-        time_till_expiry = token_body["expires_in"]
+        token = token_response["access_token"]
+        time_till_expiry = token_response["expires_in"]
         return token, start_time, time_till_expiry
     else:
         return None
